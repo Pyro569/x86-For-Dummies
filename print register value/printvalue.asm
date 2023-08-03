a1683ceb3aa48ef6ed0c960704a63d3c12e47e1b
Nathan Feiler <79991107+Pyro569@users.noreply.github.com>
2023-08-03 19:37:04 -0400

    Add files via upload

--- print register value/printvalue.asm
+++ print register value/printvalue.asm
@@ -6,10 +6,12 @@ section .text
     global _start
 
 _start:
-    mov eax, 9          ; put value of 9 in eax
-    add eax, 0x30       ; convert eax to ascii
-    mov [value], eax    ; push the eax value to the value variable
-    mov eax, value      ; move the value variable to eax
+    mov eax, 48         ; move 48 to the eax register
+    mov [value], eax    ; assign the eax register to the value pointer
+    mov eax, [value]    ; assign the value pointer to the eax register
+
+                        ; the code 48 prints out 0, but if you want to print out a larger number, say 12, you would need to run the
+                        ; print two seperate times, once using 48 in the register, and then the second time using 49 in the register
 
     mov rax, 1          ; write out the value
     mov rdi, 1
